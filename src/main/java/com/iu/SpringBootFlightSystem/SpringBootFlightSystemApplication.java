92e25b37e34c8e8a96a1bafdfb3cd3e8480e2995
Zehra Doğan <doganzehra20@gmail.com>
2022-10-03 15:59:37 +0300

    Add files via upload

---
+++ src/main/java/com/iu/SpringBootFlightSystem/SpringBootFlightSystemApplication.java
@@ -0,0 +1,13 @@
+package com.iu.SpringBootFlightSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlightSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFlightSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iu/SpringBootFlightSystem/SpringBootFlightSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iu.SpringBootFlightSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFlightSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
